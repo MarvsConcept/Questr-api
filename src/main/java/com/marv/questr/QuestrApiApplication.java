2190aa33cdbc38d2613abc9e09762dca15ad8238
MarvsConcept <Marvade8@gmail.com>
2025-11-20 16:13:42 -0800

    Initial Commit

---
+++ src/main/java/com/marv/questr/QuestrApiApplication.java
@@ -0,0 +1,13 @@
+package com.marv.questr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestrApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestrApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marv/questr/QuestrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marv.questr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestrApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
